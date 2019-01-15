bc672b08cf160964a3f8ba9997ee0667f43d0964
sasfmlzr <sasfmlzr@yandex.ru>
2019-01-15 16:06:12 +0300

    delete unused methods

--- src/main/java/com/dvachserver/testserver/ContactController.java
+++ src/main/java/com/dvachserver/testserver/ContactController.java
@@ -19,33 +19,4 @@ public class ContactController {
     public List<Contacts> all() {
         return repository.findAll();
     }
-
-    @GetMapping("/cont/get/{id}")
-    public Contacts one(@PathVariable String id) {
-
-        Contacts contacts = repository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Could not find " + id));
-
-        return contacts;
-    }
-
-    @PostMapping("/cont/new")
-    public Contacts newContacts(@RequestBody Contacts newContacts) {
-        return repository.save(newContacts);
-    }
-
-    @PutMapping("/cont/put/{id}")
-    public Contacts replaceEmployee(@RequestBody Contacts newContacts, @PathVariable String id) {
-
-        return repository.findById(id)
-                .map(contacts -> {
-                    contacts.setName(newContacts.getName());
-                    contacts.setPhone(newContacts.getPhone());
-                    contacts.setOwnerContact(newContacts.getOwnerContact());
-                    return repository.save(contacts);
-                })
-                .orElseGet(() -> {
-                    return repository.save(newContacts);
-                });
-    }
 }
